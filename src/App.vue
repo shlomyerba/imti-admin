e3dc8f0564ce41fe2cba63be30108ed82b3c9d8b
shlomyerba <shlomy71@gmail.com>
2020-11-10 08:53:59 +0200

    add futer anf scss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
+  <nav>
+    <router-link to="/">
+      <div class="navigation_logo">Imti</div>
+    </router-link>
+    <div class="navigation_user">Admin</div>
+  </nav>
   <div id="nav">
     <router-link to="/">Login</router-link> |
     <router-link to="/home">Home</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 5%;
+    background-color: blueviolet;
+    color: white;
+    font-weight: bold;
+    font-size: 24px;
+  }
 }
 
-#nav {
-  padding: 30px;
-}
+// #nav {
+//   padding: 30px;
+// }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+// #nav a {
+//   font-weight: bold;
+//   color: #2c3e50;
+// }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+// #nav a.router-link-exact-active {
+//   color: #42b983;
+// }
 </style>
