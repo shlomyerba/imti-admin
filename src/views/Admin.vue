21ec40ee4bcb436a4251150c82f4090106d63c60
shlomyerba <shlomy71@gmail.com>
2020-11-10 18:52:18 +0200

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="Admin">
     <div class="admin-panel">
-
       <div class="admin-panel_main-menu">
-        <h1>left</h1>
-        <!-- <button class="admin-panel_main-menu_button" @click="exit">
+        <button class="admin-panel_main-menu_button" @click="exit">
           Controller
         </button>
         <button class="admin-panel_main-menu_button" @click="exit">
@@ -12,16 +10,14 @@
         </button>
         <button class="admin-panel_main-menu_button" @click="exit">
           Reports
-        </button> -->
+        </button>
       </div>
 
       <div class="admin-panel_action-screen">
-                <h1>ritgh</h1>
-
-        <!-- <h1>action!</h1>
-        <h1>action!</h1>
-        <h1>action!</h1>
-        <h1>action!</h1> -->
+        <h1>action 1</h1>
+        <h1>action 2</h1>
+        <h1>action 3</h1>
+        <h1>action 4</h1>
       </div>
     </div>
   </div>
@@ -30,39 +26,36 @@
 <style lang="scss" scoped>
 .admin-panel {
   display: flex;
-    align-items: center;
-    justify-content: space-between;
-// display: flex;
-//   grid-template-columns: 1fr 3fr;
-  // width: 100%;
+  justify-content: space-between;
   padding: 50px 5%;
 
   .admin-panel_main-menu {
-    // display: grid;
-width: 30%;
-    // .admin-panel_main-menu_button {
-    //   background: none !important;
-    //   border: none;
-    //   padding: 15px !important;
-    //   color: blueviolet;
-    //   cursor: pointer;
-    //   font-weight: bold;
-    //   font-size: 24px;
-    //   border-radius: 5px;
-    //   border: 1px solid #dfe3e8;
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    padding: 10px 1%;
+    border-right: 2px solid blueviolet;
 
-      // &:hover {
-      //   opacity: 0.5;
-      // }
-    // }
+    .admin-panel_main-menu_button {
+      background: none !important;
+      border: none;
+      padding: 15px !important;
+      color: blueviolet;
+      cursor: pointer;
+      font-weight: bold;
+      font-size: 24px;
+
+      &:hover {
+        opacity: 0.5;
+      }
+    }
   }
 
   .admin-panel_action-screen {
     width: 70%;
-
-  //   display: grid;
-  //   width: 100%;
-  // padding: 50px 5%;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 15%;
   }
 }
 </style>
