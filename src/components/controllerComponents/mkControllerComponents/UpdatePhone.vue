e7c0396797e795731f16e7d88262217e665f8e72
shlomyerba <shlomy71@gmail.com>
2020-11-15 14:24:56 +0200

    Update UpdatePhone.vue

--- src/components/controllerComponents/mkControllerComponents/UpdatePhone.vue
+++ src/components/controllerComponents/mkControllerComponents/UpdatePhone.vue
@@ -1,18 +1,133 @@
 <template>
   <div class="update-phone">
-      <h1>Update Phone</h1>
-    
+<form class="update-phone_form" @submit.prevent="updatePhone">
+      <div class="update-phone_container">
+        <label>Mks *</label>
+        <select
+          id="chooseMk"
+          v-model="state.selectedMk"
+          @change="findCurrentPhone"
+          required
+        >
+          <option
+            :value="option.id"
+            v-for="(option, index) in state.Mks"
+            :key="index"
+          >
+            {{ `${option.first} ${option.last}` }}
+          </option>
+        </select>
+         <label>Phone *</label>
+        <input
+          type="number"
+          placeholder="Enter phone number"
+          v-model="state.phone"
+          required
+        />
+        <button>update</button>
+      </div>
+    </form>    
   </div>
 </template>
 
 
 <script>
+import axios from "axios";
+import VueCookies from "vue-cookies";
+import { reactive, onMounted } from "vue";
+import { baseUrl } from "../../../assets/url";
+
 export default {
   name: "UpdatePhone",
+  setup() {
+    const state = reactive({
+      selectedMk: null,
+      Mks: [],
+      phone: "",
+    });
+
+    async function findCurrentPhone() {
+      let token = VueCookies.get("token");
+      let url = `${baseUrl}/admin/report/mk/info?imageIncluded=false&mkId=${state.selectedMk}&uuid=${token}`;
+      console.log(url);
+      try {
+        let response = await axios.get(url);
+        state.phone = response.data.phone;
+        console.log("response", response);
+      } catch (e) {
+        console.log("e", e);
+      }
+    }
+
+    async function updatePhone() {
+      let token = VueCookies.get("token");
+      let url = `${baseUrl}/admin/mk/update/phone?phone=${state.phone}&mkId=${state.selectedMk}&uuid=${token}`;
+      console.log(url);
+      try {
+        let response = await axios.get(url);
+        state.selectedMk = "";
+        state.phone = "";
+        console.log("response", response);
+      } catch (e) {
+        console.log("e", e);
+      }
+    }
+
+    onMounted(async () => {
+      let token = await VueCookies.get("token");
+      let url = `${baseUrl}/admin/report/mk/all?imageIncluded=false&uuid=${token}`;
+      console.log(url);
+      try {
+        let response = await axios.get(url);
+        console.log(response);
+        if (response.data) {
+          state.Mks = response.data;
+        }
+      } catch (e) {
+        console.log("e", e);
+      }
+    });
+
+    return {
+      state,
+      updatePhone,
+      findCurrentPhone,
+    };
+  },
 };
 </script>
 
   
 <style lang="scss" scoped>
+.update-phone_container {
+  padding: 16px;
+  font-weight: bold;
 
+  input,
+  select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  option {
+    font-size: 15px;
+  }
+  button {
+    background-color: blueviolet;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    font-weight: bold;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
 </style>
