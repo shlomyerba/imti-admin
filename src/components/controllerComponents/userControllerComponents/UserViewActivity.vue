eede0f800eac8a93d9130c94fb6e923707a9ad3e
shlomyerba <shlomy71@gmail.com>
2020-11-17 15:49:20 +0200

    table is working

--- src/components/controllerComponents/userControllerComponents/UserViewActivity.vue
+++ src/components/controllerComponents/userControllerComponents/UserViewActivity.vue
@@ -17,7 +17,19 @@
     </form>
   </div>
   <div>
-    <b-table striped hover :items="items"></b-table>
+    <table-lite
+      :has-checkbox="true"
+      :is-loading="table.isLoading"
+      :is-re-search="table.isReSearch"
+      :columns="table.columns"
+      :rows="table.rows"
+      :total="table.totalRecordCount"
+      :sortable="table.sortable"
+      :messages="table.messages"
+      @do-search="doSearch"
+      @is-finished="tableLoadingFinish"
+      @return-checked-rows="updateCheckedRows"
+    ></table-lite>
   </div>
 </template>
 
@@ -27,11 +39,79 @@ import axios from "axios";
 import VueCookies from "vue-cookies";
 import { reactive, onMounted } from "vue";
 import { baseUrl } from "../../../assets/url";
+import TableLite from "vue3-table-lite";
 
 export default {
   name: "UserViewActivity",
+  components:{TableLite},
   setup() {
-  
+    const table = reactive({
+      isLoading: false,
+      isReSearch: false,
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          width: "3%",
+          sortable: true,
+          isKey: true,
+        },
+        {
+          label: "Name",
+          field: "name",
+          width: "10%",
+          sortable: true,
+          display: function (row) {
+            return (
+              '<a href="#" data-id="' +
+              row.user_id +
+              '" class="name-btn">' +
+              row.name +
+              "</button>"
+            );
+          },
+        },
+        {
+          label: "Email",
+          field: "email",
+          width: "15%",
+          sortable: true,
+        },
+        {
+          label: "",
+          field: "quick",
+          width: "10%",
+          display: function (row) {
+            return (
+              '<button type="button" data-id="' +
+              row.user_id +
+              '" class="quick-btn">Button</button>'
+            );
+          },
+        },
+      ],
+      rows: [
+        {
+          id: 1,
+          name: "TEST1",
+        },
+        {
+          id: 2,
+          name: "TEST2",
+        },
+      ],
+      totalRecordCount: 2,
+      sortable: {
+        order: "id",
+        sort: "asc",
+      },
+      messages: {
+        pagingInfo: "Showing {0}-{1} of {2}",
+        pageSizeChangeLabel: "Row count:",
+        gotoPageLabel: "Go to page:",
+        noDataAvailable: "No data",
+      },
+    });
     const state = reactive({
       selectedUser: null,
       users: [],
@@ -68,6 +148,7 @@ export default {
     return {
       state,
       viewUserActivity,
+      table
     };
   },
 };
