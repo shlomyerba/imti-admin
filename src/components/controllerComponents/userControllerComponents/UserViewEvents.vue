7fbbead13b382d2aff7dc1170b141f33c2a91ace
shlomyerba <shlomy71@gmail.com>
2020-11-18 15:02:36 +0200

    add all user view

--- src/components/controllerComponents/userControllerComponents/UserViewEvents.vue
+++ src/components/controllerComponents/userControllerComponents/UserViewEvents.vue
@@ -1,16 +1,148 @@
 <template>
   <div class="user-view-events">
-    <h1>User View Events</h1>
+    <form class="user-view-events_form" @submit.prevent="viewUserEvents">
+      <div class="user-view-events_container">
+        <label>Users *</label><br />
+        <select id="ChooseUser" v-model="state.selectedUser" required>
+          <option
+            :value="option.id"
+            v-for="(option, index) in state.users"
+            :key="index"
+          >
+            {{ `${option.first} ${option.last}` }}
+          </option>
+        </select>
+        <button>view</button>
+      </div>
+    </form>
+  </div>
+  <div v-if="state.events.length">
+    <table class="event_info">
+      <tr>
+        <th>Evevt</th>
+        <th>Date</th>
+      </tr>
+      <tr v-for="(event, index) in state.events" :key="index">
+        <td>{{ event.title }}</td>
+        <td>{{ findDate(event.timestamp) }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 
 <script>
+import axios from "axios";
+import VueCookies from "vue-cookies";
+import { reactive, onMounted } from "vue";
+import { baseUrl } from "../../../assets/url";
+
 export default {
-  name: "UserViewEvents",
+  name: "ViewUserEvents",
+  setup() {
+    const state = reactive({
+      selectedUser: null,
+      users: [],
+      events: [],
+    });
+
+    async function viewUserEvents() {
+      let token = VueCookies.get("token");
+      let url = `${baseUrl}/admin/user/view/events?userId=${state.selectedUser}&uuid=${token}`;
+      try {
+        let response = await axios.get(url);
+        state.events = response.data;
+        console.log("response", response);
+      } catch (e) {
+        console.log("e", e);
+      }
+    }
+
+    function findDate(timestamp) {
+      let date = new Date(timestamp);
+      console.log("date", date);
+      let day = date.getDay();
+      if (date.getDay() < 10) {
+        day = `0${date.getDay()}`;
+      }
+      let month = date.getMonth() + 1;
+      if (date.getMonth() + 1 < 10) {
+        month = `0${date.getMonth() + 1}`;
+      }
+      return `${day}-${month}-${date.getFullYear()}`;
+    }
+    onMounted(async () => {
+      let token = VueCookies.get("token");
+      let url = `${baseUrl}/admin/report/user/all?uuid=${token}`;
+      try {
+        let response = await axios.get(url);
+        if (response.data) {
+          state.users = response.data;
+        }
+      } catch (e) {
+        console.log("e", e);
+      }
+    });
+
+    return {
+      state,
+      viewUserEvents,
+      findDate,
+    };
+  },
 };
 </script>
 
   
 <style lang="scss">
+.user-view-events_container {
+  padding: 16px;
+  font-weight: bold;
+
+  select {
+    width: 45%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  option {
+    font-size: 15px;
+  }
+  button {
+    background-color: blueviolet;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 45%;
+    font-weight: bold;
+    margin-left: 5%;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid blueviolet;
+  text-align: center;
+  padding: 8px;
+}
+th {
+  font-size: 20px;
+}
+tr:nth-child(even) {
+  background-color: blueviolet;
+  color: white;
+}
 </style>
--- src/components/controllerComponents/userControllerComponents/UserViewInfo.vue
+++ src/components/controllerComponents/userControllerComponents/UserViewInfo.vue
@@ -1,16 +1,153 @@
 <template>
   <div class="user-view-info">
-    <h1>User View Info</h1>
+    <form class="user-view-info_form" @submit.prevent="userViewInfo">
+      <div class="user-view-info_container">
+        <label>Users *</label><br />
+        <select id="ChooseUser" v-model="state.selectedUser" required>
+          <option
+            :value="option.id"
+            v-for="(option, index) in state.users"
+            :key="index"
+          >
+            {{ `${option.first} ${option.last}` }}
+          </option>
+        </select>
+        <button>view</button>
+      </div>
+    </form>
+  </div>
+  <div v-if="state.info">
+    <table class="user_info">
+      <tr>
+        <th>Id</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Membership</th>
+        <th>Password</th>
+        <th>Phone</th>
+      </tr>
+      <tr>
+        <td>{{ state.info.id }}</td>
+        <td>{{ state.info.first }}</td>
+        <td>{{ state.info.last }}</td>
+        <td>{{ state.info.email }}</td>
+        <td>{{ state.info.membership }}</td>
+        <td>{{ state.info.password }}</td>
+        <td>{{ state.info.phone }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 
 <script>
+import axios from "axios";
+import VueCookies from "vue-cookies";
+import { reactive, onMounted } from "vue";
+import { baseUrl } from "../../../assets/url";
+
 export default {
   name: "UserViewInfo",
+  setup() {
+    const state = reactive({
+      selectedUser: null,
+      users: [],
+      info: "",
+    });
+
+    async function userViewInfo() {
+      let token = VueCookies.get("token");
+      let url = `${baseUrl}/admin/user/view/info?userId=${state.selectedUser}&uuid=${token}`;
+      try {
+        let response = await axios.get(url);
+        state.info = response.data;
+        console.log("response", response);
+        console.log(state.info);
+      } catch (e) {
+        console.log("e", e);
+      }
+    }
+
+    onMounted(async () => {
+      let token = VueCookies.get("token");
+      let url = `${baseUrl}/admin/report/user/all?uuid=${token}`;
+      try {
+        let response = await axios.get(url);
+        if (response.data) {
+          state.users = response.data;
+        }
+      } catch (e) {
+        console.log("e", e);
+      }
+    });
+
+    return {
+      state,
+      userViewInfo,
+    };
+  },
 };
 </script>
 
   
 <style lang="scss">
+.user-view-info_container {
+  padding: 16px;
+  font-weight: bold;
+
+  select {
+    width: 45%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  option {
+    font-size: 15px;
+  }
+  button {
+    background-color: blueviolet;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 45%;
+    font-weight: bold;
+    margin-left: 5%;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th {
+  border: 1px solid blueviolet;
+  text-align: center;
+  padding: 8px;
+  background-color: blueviolet;
+  color: white;
+  font-size: 14px;
+}
+
+td {
+  border: 1px solid blueviolet;
+  text-align: center;
+  padding: 8px;
+  font-size: 14px;
+
+}
+
+tr:nth-child(even) {
+  background-color: white;
+  color: black;
+}
 </style>
