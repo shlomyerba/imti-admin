560cae7a6fbba1faae3bc068874a949252e35842
shlomyerba <shlomy71@gmail.com>
2020-11-25 11:07:07 +0200

    Create reportUserOptions.js

---
+++ src/assets/report/reportUserOptions.js
@@ -0,0 +1,3 @@
+export const reportUserOptions = [{ id: "userActivity", name: "פעילות משתמש" }, { id: "all", name: "הכל" }, { id: "byEvent", name: "לפי ארוע" },
+{ id: "allEvents", name: "כל ארועי המשתמש" }, { id: "specificUserInfo", name: "פרטי משתמש ספציפי" },];
+
