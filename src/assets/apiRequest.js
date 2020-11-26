15019ad7655ea7edcaf1f5af797f085b68b76baf
shlomyerba <shlomy71@gmail.com>
2020-11-26 11:05:19 +0200

    Update apiRequest.js

--- src/assets/apiRequest.js
+++ src/assets/apiRequest.js
@@ -2,9 +2,8 @@ import axios from "axios";
 import VueCookies from "vue-cookies";
 import { baseUrl } from "./url";
 
-export async function getAllEvents() {
-    let token = await VueCookies.get("token");
-    let url = `${baseUrl}/admin/report/event/all?uuid=${token}`;
+
+export async function generalReturnList(url) {
     try {
         let response = await axios.get(url);
         if (response.data) {
@@ -13,48 +12,32 @@ export async function getAllEvents() {
         }
     } catch (e) {
         console.log("e", e);
+        return null;
     }
+}
+
+export async function getAllEvents() {
+    let token = await VueCookies.get("token");
+    let url = `${baseUrl}/admin/report/event/all?uuid=${token}`;
+    return await generalReturnList(url);
 }
 
 export async function getAllParties() {
     let token = await VueCookies.get("token");
     let url = `${baseUrl}/admin/report/party/all?uuid=${token}`;
-    try {
-        let response = await axios.get(url);
-        if (response.data) {
-            console.log(response.data);
-            return response.data;
-        }
-    } catch (e) {
-        console.log("e", e);
-    }
+    return await generalReturnList(url);
 }
 
 
 export async function getAllMks() {
     let token = await VueCookies.get("token");
     let url = `${baseUrl}/admin/report/mk/all?imageIncluded=false&uuid=${token}`;
-    try {
-        let response = await axios.get(url);
-        if (response.data) {
-            console.log(response.data);
-            return response.data;
-        }
-    } catch (e) {
-        console.log("e", e);
-    }
+    return await generalReturnList(url);
 }
 
 
 export async function getAllUsers() {
     let token = VueCookies.get("token");
     let url = `${baseUrl}/admin/report/user/all?uuid=${token}`;
-    try {
-        let response = await axios.get(url);
-        if (response.data) {
-            return response.data;
-        }
-    } catch (e) {
-        console.log("e", e);
-    }
+    return await generalReturnList(url);
 }
